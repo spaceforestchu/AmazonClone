9622e127ce9afdc4277f8103a7a01644b010accc
SpaceForestChu <spaceforestchu@gmail.com>
2016-07-07 20:08:34 -0400

    routes main and user added

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ mongoose.connect('mongodb://root:abc123@ds015915.mlab.com:15915/ecommerce', func
 //Middleware
 //add how to use static page
 app.use(express.static(__dirname + '/public'));
-
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -28,31 +27,10 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 
-
-
-app.post('/create-user', function(req, res, next){
-  var user = new User();
-
-  user.profile.name = req.body.name;
-  user.password = req.body.password;
-  user.email = req.body.email;
-
-  user.save(function(err){
-    if(err) return next(err);
-
-    res.json("Sucessfully created user");
-  });
-});
-
-
-app.get('/', function(req, res){
-  res.render("main/home");
-});
-
-app.get('/about', function(req, res){
-  res.render('main/about');
-});
-
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+app.use(mainRoutes);
+app.use(userRoutes);
 
 app.listen(3000, function(err){
   if(err) throw err;
